c8f05ce87561385e023d9450fa9bea32526517e9
Sotatek-TrangNguyen5 <trang.nguyen5@sotatek.com>
2021-10-05 21:32:09 +0700

    save

--- src/main/java/dictionaryv4/Controller/DepartmentCotroller.java
+++ src/main/java/dictionaryv4/Controller/DepartmentCotroller.java
@@ -7,18 +7,11 @@ import javax.websocket.server.PathParam;
 import dictionaryv4.Entity.Department;
 import dictionaryv4.Service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "departments")
+@CrossOrigin(origins = "*")
 public class DepartmentCotroller {
 	
 	@Autowired
